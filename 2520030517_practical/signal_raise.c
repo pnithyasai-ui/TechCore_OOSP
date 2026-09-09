35cd246ff697c3fa048a1eb7dad0900315aa80d4
pnithyasai-ui <p.nithyasai@klh.edu.in>
2026-09-09 05:13:00 +0000

    Add signal raise practical

---
+++ 2520030517_practical/signal_raise.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void handler(int signo) {
+    printf("signal received\n");
+    printf("signal no: %d\n", signo);
+    exit(1);
+}
+
+int main() {
+    signal(SIGINT, handler);
+
+    while (1) {
+        printf("infinite no of prints\n");
+        break;
+    }
+
+    raise(SIGINT);
+
+    return 0;
+}
+
